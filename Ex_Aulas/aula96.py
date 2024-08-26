e241938a77259608f50279544a3244a0b059bb69
igorvianaf <igor.vianaf1@hotmail.com>
2024-08-26 11:53:40 -0300

    importando modulos

---
+++ Ex_Aulas/aula96.py
@@ -0,0 +1,7 @@
+from sys import exit, platform
+# import sys as s
+#from sys import plataform as ptf
+#from sys import exit as ex
+
+
+print(platform)
---
+++ Ex_Aulas/aula97.py
@@ -0,0 +1,6 @@
+import aula97_m
+from aula97_m import variavel_aleatoria
+
+print('Este módulo se chama', __name__)
+print(aula97_m.variavel_aleatoria)
+print(variavel_aleatoria)
