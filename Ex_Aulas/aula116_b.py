a125c88e7618c255fd84e645e4b8c2bb5102710b
igorvianaf <igor.vianaf1@hotmail.com>
2024-09-11 22:53:32 -0300

    write and read arquivo.txt

--- Ex_Aulas/aula116_b.py
+++ Ex_Aulas/aula116_b.py
@@ -1,4 +1,8 @@
 caminho = 'aula116.txt'
 
 with open(caminho, 'w') as arquivo:
-    ...
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+
+with open(caminho, 'r') as arquivo:
+    print(arquivo.read())
