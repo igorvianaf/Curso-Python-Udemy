340ccfd75c12bbaa7cf0439611cc0d324129b676
igorvianaf <igor.vianaf1@hotmail.com>
2024-09-19 21:09:02 -0300

    inserir funções
    adicionar, desfazer e refazer alterações na lista

--- Ex_Aulas/ListaDeTarefas.py
+++ Ex_Aulas/ListaDeTarefas.py
@@ -1,30 +1,62 @@
 import os
 
 
-def mostrar_lista(lista):
+lista = []
+lista_refazer = []
+
+
+def listar_tarefas(lista):
+    if not lista:
+        print('Sua lista de tarefas está vazia!')
+        return
     for item in lista:
-            print(item)
+        print(item)
+
+def desfazer_lista(lista, lista_desfazer):
+    if not lista:
+        print('Não temos tarefas para desfazer na sua lista')
+        return
+    ultima_tarefa = lista.pop()
+    lista_desfazer.append(ultima_tarefa)
+    print(f'{ultima_tarefa} foi removido da sua lista')
+
+def refazer_lista(lista, lista_refazer):
+    if not lista_refazer:
+        print('Não temos tarefas para refazer na sua lista')
+        return
+    ultima_tarefa = lista_refazer.pop()
+    lista.append(ultima_tarefa)
+    print(f'{ultima_tarefa} voltou a sua lista')
+
+def adicionar(tarefa, lista):
+    lista.append(tarefa)
+    print(f'{tarefa} adicionado a sua lista')
 
 def opcoes(entrada_usuario):
-    if entrada_usuario == 'listar':
-        mostrar_lista(tarefas)
-    elif entrada_usuario == 'desfazer':
-        ...
-    elif entrada_usuario == 'refazer':
-        ...
-    elif entrada_usuario == 'clear':
+    var_checar = entrada_usuario.strip().lower()
+
+    if var_checar == 'listar':
+        listar_tarefas(lista)
+
+    elif var_checar == 'desfazer':
+        desfazer_lista(lista, lista_refazer)
+
+    elif var_checar == 'refazer':
+        refazer_lista(lista, lista_refazer)
+
+    elif var_checar == 'clear':
         os.system('cls')
-    elif entrada_usuario == 'sair':
+
+    elif var_checar == 'sair':
         return False
+
     else:
-        tarefas.append(entrada_usuario)
+        adicionar(entrada_usuario, lista)
 
 
-tarefas = []
-
-
+print('Bem vindo a sua lista!!')
 while True:
-    print('Comandos: listar, desfazer e refazer')
-    menu = opcoes(input('Digite uma tarefa ou comando: ').lower().strip())
+    print('Digite um dos comandos (Listar, Desfazer, Refazer) ou adicione algo a sua lista: ')
+    menu = opcoes(input('Digite uma tarefa ou comando: '))
     if menu == False:
         break
