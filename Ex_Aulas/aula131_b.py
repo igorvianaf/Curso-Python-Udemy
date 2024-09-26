c56dd28c8adb716c873ce796f1df54cc0f7d1570
igorvianaf <igor.vianaf1@hotmail.com>
2024-09-26 00:25:22 -0300

    Update aula131_b.py

--- Ex_Aulas/aula131_b.py
+++ Ex_Aulas/aula131_b.py
@@ -1,17 +1,33 @@
 class Caneta:
     def __init__(self, cor):
-        self.cor_tinta = cor
+        self._cor = cor
+        self._cor_tampa = None
 
     @property
     def cor(self):
         print('property')
-        return self.cor_tinta
+        return self._cor
+
+    @cor.setter
+    def cor(self, valor):
+        if valor == 'rosa':
+            raise ValueError('A cor informada não é aceita')
+        self._cor = valor
 
     @property
-    def marca(self):
-        print('Novo property')
-        return 'Bic'
+    def cor_tampa(self):
+        return self._cor_tampa
+
+    @cor_tampa.setter
+    def cor_tampa(self, valor):
+        self._cor_tampa = valor
+
 
 caneta = Caneta('Azul')
-print(caneta.cor)
-print(caneta.marca)
+print(caneta._cor)
+
+caneta.cor = 'preto'
+print(caneta._cor)
+
+caneta._cor_tampa = 'vermelho'
+print(caneta._cor_tampa)
